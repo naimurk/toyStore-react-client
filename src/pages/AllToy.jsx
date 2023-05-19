cd3e658288fe56075f749752cab53ee199f7ab57
naimurk <naimurk781@gmail.com>
2023-05-19 15:19:22 +0600

    serch funtion implement

--- src/pages/AllToy.jsx
+++ src/pages/AllToy.jsx
@@ -3,19 +3,34 @@ import { useState, useEffect } from 'react';
 import SinglePostedToy from './SinglePostedToy';
 const AllToy = () => {
     const [allToys, setAllToys] = useState([]);
+    const [searchText , setSearchText] = useState('')
     useEffect(() => {
         fetch('http://localhost:5000/postedToy')
             .then(res => res.json())
             .then(data => setAllToys(data))
     }, [])
+    
+    const handleSearch = () => {
+       
+        fetch(`http://localhost:5000/getJobsByText/${searchText}`)
+        .then(res => res.json())
+        .then(data => setAllToys(data))
+        
+    }
+
+    console.log(searchText);
     return (
         <div className='mb-11'>
             <h1 className='text-6xl py-24 text-center font-semibold'>All Posted Action Figures</h1>
+            <div className='flex justify-center pb-11 gap-x-3'>
+                <input onChange={(e)=> setSearchText(e.target.value)} type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                <button onClick={handleSearch} className='btn btn-warning'>serach here</button>
+            </div>
             <table className="table w-full">
                 {/* head */}
                 <thead>
                     <tr>
-                      
+
                         <th>Toy name</th>
                         <th>category</th>
                         <th>price</th>
@@ -36,15 +51,15 @@ const AllToy = () => {
                 </tbody>
                 {/* foot */}
                 <tfoot>
-                <tr>
-                      
-                      <th>Toy name</th>
-                      <th>category</th>
-                      <th>price</th>
-                      <th>quantity</th>
-                      <th>seller email</th>
-                      <th>Details</th>
-                  </tr>
+                    <tr>
+
+                        <th>Toy name</th>
+                        <th>category</th>
+                        <th>price</th>
+                        <th>quantity</th>
+                        <th>seller email</th>
+                        <th>Details</th>
+                    </tr>
                 </tfoot>
 
             </table>
--- src/pages/MyPost.jsx
+++ src/pages/MyPost.jsx
@@ -1,60 +1,65 @@
 import React from 'react';
-import { useContext,useEffect,useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContex } from '../AuthProvider/AuthProvider';
 import SingleMyPosted from './SingleMyPosted';
 
 const MyPost = () => {
-    const {user} = useContext(AuthContex)
-    const [posted,setPosted] = useState([])
+    const { user } = useContext(AuthContex)
+    const [posted, setPosted] = useState([])
     // console.log(user?.email);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`http://localhost:5000/myPosted/${user?.email}`)
-        .then(res => res.json())
-        .then(data => setPosted(data))
-    },[user])
+            .then(res => res.json())
+            .then(data => setPosted(data))
+    }, [user])
+
+
     return (
         <div className='mb-11'>
-        <h1 className='text-6xl py-24 text-center font-semibold'>My posted Everything</h1>
-        <table className="table w-full">
-            {/* head */}
-            <thead>
-                <tr>
-                  
-                    <th>Toy name</th>
-                    <th>category</th>
-                    <th>price</th>
-                    <th>quantity</th>
-                    <th>seller email</th>
-                    <th>Details</th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* row 1 */}
-                {
-                    posted && posted.map(singleMyPosted => <SingleMyPosted
-                    key={singleMyPosted._id}
-                    singleMyPosted = {singleMyPosted}
-                    >
+            <h1 className='text-6xl py-24 text-center font-semibold'>My posted Everything</h1>
+            {/* <div className='search-box'>
+                <input type="text" placeholder="Type here" className="input w-full max-w-xs" />
+            </div> */}
+            <table className="table w-full">
+                {/* head */}
+                <thead>
+                    <tr>
 
-                    </SingleMyPosted>)
-                }
+                        <th>Toy name</th>
+                        <th>category</th>
+                        <th>price</th>
+                        <th>quantity</th>
+                        <th>seller email</th>
+                        <th>Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                    {
+                        posted && posted.map(singleMyPosted => <SingleMyPosted
+                            key={singleMyPosted._id}
+                            singleMyPosted={singleMyPosted}
+                        >
 
-            </tbody>
-            {/* foot */}
-            <tfoot>
-            <tr>
-                  
-                  <th>Toy name</th>
-                  <th>category</th>
-                  <th>price</th>
-                  <th>quantity</th>
-                  <th>seller email</th>
-                  <th>Details</th>
-              </tr>
-            </tfoot>
+                        </SingleMyPosted>)
+                    }
 
-        </table>
-    </div>
+                </tbody>
+                {/* foot */}
+                <tfoot>
+                    <tr>
+
+                        <th>Toy name</th>
+                        <th>category</th>
+                        <th>price</th>
+                        <th>quantity</th>
+                        <th>seller email</th>
+                        <th>Details</th>
+                    </tr>
+                </tfoot>
+
+            </table>
+        </div>
     );
 };
 
